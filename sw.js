7dd9f51ab76e28427f52d08d4c8a2c096cc585ec
marselianx <sk9dlolol@gmail.com>
2026-07-01 16:38:40 +0100

    hh

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11225371
+    "zoneId": 11225423
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
